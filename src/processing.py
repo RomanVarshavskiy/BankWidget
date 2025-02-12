d6458257733a639bf6861d6e5b2528ac1374d169
Roman Varshavski <raman.varshavskiy@gmail.com>
2025-02-12 22:44:49 +0100

    update processing file add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -3,3 +3,10 @@ def filter_by_state(my_list: list, state="EXECUTED") -> list:
     filter_list = [d for d in my_list if d["state"] == state]
 
     return filter_list
+
+
+def sort_by_date(list_dict: list) -> list:
+    """Функция сортирует список словарей по значению ключа date"""
+    sorted_list = sorted(list_dict, key=lambda d: d["date"], reverse=True)
+
+    return sorted_list
