7d140c34dcc9d7f56573a99856f188325d3c56b9
Roman Varshavski <raman.varshavskiy@gmail.com>
2025-03-12 07:27:00 +0100

    add utils.py + operations.json

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import json
+from typing import Dict, Any, List
+
+from config import PATH_DIR
+
+
+def get_data_transactions(path: str) -> List[Dict[str, Any]]:
+    """Функция возвращает список словарей с данными о финансовых транзакциях"""
+    try:
+        with open(PATH_DIR / f"data/{path}", "r", encoding="utf-8") as transaction_file:
+            try:
+                transactions = json.load(transaction_file)
+            except json.JSONDecodeError:
+                print("Ошибка декодирования файла")
+                return []
+    except FileNotFoundError:
+        print(f"Файл {path} не найден")
+        return []
+    if not isinstance(transactions, list):
+        return []
+
+    return transactions
+
+print(get_data_transactions("operations.json"))
+print(get_data_transactions("empty2.json"))
